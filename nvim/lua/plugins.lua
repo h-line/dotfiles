d65634f21917eb2dee9a5c149b5e85bd1fe611db
Lauri Hälinen <lauri.halinen@iki.fi>
2023-01-19 23:50:58 +0200

    add lua language server

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -44,6 +44,24 @@ end)
 
 require('lspconfig').tsserver.setup{}
 require('lspconfig').eslint.setup{}
+require('lspconfig').sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 require('lualine').setup {
   options = {
